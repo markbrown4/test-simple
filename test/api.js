9c7f0d82cdf71fcf8030f532ad461c3b71221e22
Mark Brown <markbrown4@gmail.com>
2016-08-06 17:02:06 +0100

    fetch-mock over sinon

--- test/api.js
+++ test/api.js
@@ -1,31 +1,24 @@
 
 import assert from 'assert';
-import sinon from 'sinon';
+import fetchMock from 'fetch-mock';
 import API from '../src/lib/api';
-import { jsonResponse } from './test_helper';
 
 describe('API.get(path)', ()=> {
   it('should return a Promise with cats', (done)=> {
 
-    sinon.stub(global, 'fetch', (url)=> {
-      if (url == '/api/cats.json') {
-        return jsonResponse({
-          cats: [{
-            url: 'lolcat.jpg'
-          },{
-            url: 'dancing_pug.gif'
-          }]
-        });
-      }
-    });
+    fetchMock.get('/api/cats.json', {
+      cats: [{
+        url: 'lolcat.jpg'
+      },{
+        url: 'dancing_pug.gif'
+      }]
+    })
 
     API.get('cats.json').then((resp)=> {
       assert(resp.cats.length === 2);
       assert(resp.cats[0].url === 'lolcat.jpg');
       done();
     })
-
-    global.fetch.restore();
 
   });
 });
